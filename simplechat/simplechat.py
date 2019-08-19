c4b2887469c2390a5deb8d3874987d1b0d1e6476
Josh <jakgaming100@gmail.com>
2019-08-19 14:00:15 -0400

    Finished Simple ChatBot
    There may be bugs.

--- simplechat/simplechat.py
+++ simplechat/simplechat.py
@@ -1,21 +1,55 @@
+
+import re
+
 prompts = {
     "what": "What is a Video Game Console?",
-    "use": "What are Video Game Consoles used for?",
-    "around": "Around what time did the first video games come out.",
-    "when": "When did the NES release.",
-    "best": "What is the best selling video game console."
+    "use": "What are Video Game Consoles used for",
+    "around": "Around what time did the first video games come out",
+    "when": "When did the NES release in Japan",
+    "best": "What is the best selling video game console"
 }
 
-response = {
+responses = {
     "what": "A video game console is a computer device that outputs a video signal or visual image to display a video game that one or more people can play.",
-    "use": ""
-    "around": "The first video games appeared in the 1960s."
-    "when": ""
+    "use": "Video Game Console are used for video games to run on.",
+    "around": "The first video games appeared in the 1960s.",
+    "when": "In 1983, Nintendo released the Family Computer/Famicom/NES in America.",
+    "best": "The Sony Playstation 2 is the Best Selling Video Game Console"
 }
 
 def processInput(userInput):
-    pass
+    userInput = re.sub(r'[^\w\s]', '', userInput)
 
+    words = userInput.split(" ")
+    #print(words) 
+    matchingKeys = []
+
+    for word in words:
+        if word in responses.keys():
+            matchingKeys.append(word) 
+
+    if len(matchingKeys) == 0:
+        return "I don't know that."
+    elif len(matchingKeys) == 1:
+        return responses[matchingKeys[0]],
+    else:
+        print("I am not sure what you mean. Did you mean: ")
+        index = 1
+
+        for key in matchingKeys:
+            print(str(index) + ": " + prompts[key])
+            index += 1
+
+        valid = False
+        
+        while not valid:
+            selected = int(input("#: "))
+
+            if selected <= len(matchingKeys) and selected > 0:
+                valid = True
+            else:
+                print("Please enter one of the above.")
+        return responses[matchingKeys[selected - 1]]
 def main():
     print("Welcome to Video Game Console Facts! I can talk to you about Video Game Consoles!\n")
 
